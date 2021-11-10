ddf3409bf22be870cff4441217b53d216ae64532
rslaporte <rafanex@gmail.com>
2021-11-10 02:36:23 -0300

    Save the whales again

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1 className="text">ROCKY TAGS</h1>
+      <h1 className="text">SAVE THE WHALES</h1>
       <div data-product-id="111111" data-product-name='Eric Cartman' data-product-brand='South Park' data-product-price="0" data-product-position="1" data-product-category="Evil">
           <img className="img" alt='Cartman' src={imgUrl} />
           <button className='button' data-button-prop='Property 1' data-button-prop2='Property 2' data-button-prop3='Property 3'>Click</button>
